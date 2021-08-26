4154e2508078c9eb6a5e502ae8300bbad5b577ec
mmagico571@gmail.com <mmagico571@gmail.com>
2021-08-25 19:02:24 -0500

    tipos de datos 1

--- programas/dataTypes.py
+++ programas/dataTypes.py
@@ -1,4 +1,3 @@
-
 #string
 print("Primer texto")
 print('Segundo texto')
@@ -7,7 +6,43 @@ print('''Cuarto texto''')
 
 # mostrar el tipo de datos
 print(type("texto"))
-
+print(type(100))
+print(type(1.1))
+print(type(True))
+print(type(False))
+print(type([10, 3.14,"Hola",True]))
+print(type((10, 3.14,"Hola",True)))
+print(type({"nombre":"tobias", "aPaterno":"piña", "aMaterno":"posos","edad":5}))
+print(type(None))
 # concatenar textos
 print("texto 1 " + "texto 2")
 
+# -> 35:00
+
+# Numeros
+    # Enteros
+print(30)
+    # Flotantes
+print(3.14)
+
+# Booleanos
+print(False)
+print(True)
+
+# Listas
+[10,20,30,40]
+["Hello","Bye","hola","adios"]
+[10, 3.14,"Hola",True]
+[]
+# Tuplas
+(10,20,30,40)
+("Hello","Bye","hola","adios")
+(10, 3.14,"Hola",True)
+()
+
+# Diccionarios
+
+{"nombre":"tobias", "aPaterno":"piña", "aMaterno":"posos","edad":5}
+
+# Dato sin definir
+None
