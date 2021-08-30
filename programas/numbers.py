5a40ac68f5f0c7f6a27e42ee2b98666b14edd19f
mmagico571@gmail.com <mmagico571@gmail.com>
2021-08-30 18:05:58 -0500

    numbers 1

---
+++ programas/numbers.py
@@ -0,0 +1,48 @@
+# Numbers
+# ->01:27:00
+
+# Numeros enteros
+entero = 100
+print(type(entero))
+print(entero)
+
+# Numeros flotantes
+flotante = 3.14
+print(type(flotante))
+print(flotante)
+
+# Operaciones aritmeticas
+    # Suma
+print(7+2)
+    # Resta
+print(7-2)
+    # Multiplicacion
+print(7*2)
+    # Division
+print(7/2)
+    # Modulo
+print(7%2)
+    # Division entera
+print(7//2)
+    # Potencia de un numero
+print(2**3)
+
+# Precedencia de operadores
+    # parentesis
+    # potencia
+    # multiplicaciones, diviciones
+    # sumas y restas
+
+# Entrada de datos en python
+numero = input("Ingrese un numero: ")
+print(numero)
+
+# Convertir string a enteros o flotantes
+print(type(numero))
+suma = int(numero) + 5
+print(suma)
+print(type(suma))
+
+numero2 = float(input("Ingresa otro numero: "))
+print(type(numero2))
+print(numero2)
