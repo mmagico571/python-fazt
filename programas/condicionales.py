3532ff6d2fee58203f69050b0fac83eada28494c
mmagico571@gmail.com <mmagico571@gmail.com>
2021-09-05 14:13:16 -0500

    condicionales 2

--- programas/condicionales.py
+++ programas/condicionales.py
@@ -1,3 +1,30 @@
 # Condicionales
     #->02:19:50
-    
+
+# Operadores de comparacion
+    # mayor que >
+    # menor que <
+    # igual que ==
+
+# Estructura if
+edad = 20
+if edad > 30:
+    print("mayor que treinta")
+else:
+    print("Menor que treinta")
+
+color = "red"
+
+if color == "blue":
+    print("blue")
+else:
+    print("Otro color")
+
+# Estructura elif
+
+if color == "blue":
+    print('blue')
+elif color == "red":
+    print("red")
+else:
+    print("Otro color")
