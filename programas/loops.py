333c8bc59afa4a8c476ecb575cee8318155553ce
mmagico571@gmail.com <mmagico571@gmail.com>
2021-12-10 20:08:06 -0600

    211210a

--- programas/loops.py
+++ programas/loops.py
@@ -1,4 +1,14 @@
 # Loops
     # -> 2:36:30
 
-# 
+# for
+
+for numero in range(1,8):
+    print(numero)
+
+# while
+
+contador = 100
+while contador <= 110:
+    print(contador)
+    contador = contador + 1
